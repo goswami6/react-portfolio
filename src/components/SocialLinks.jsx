74e626a6ed19dda17b2f31e275e971b207aea0cd
Akanksha Goswami <107531046+goswami6@users.noreply.github.com>
2023-06-19 00:45:11 -0400

    first commit

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {FaGithub, FaLinkedin  } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
 
